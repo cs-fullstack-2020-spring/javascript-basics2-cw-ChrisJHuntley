6bc250bc0996223f81c1c8f6aea8153803b70ebe
chris huntley <cjhuntley4@gmail.com>
2020-02-14 14:27:06 -0600

    Almost had it

---
+++ index.js
@@ -0,0 +1,28 @@
+let Name="My Name Is"
+let Chris="Chris"
+console.log( Name+" "+Chris)
+alert(Name+ " "+ Chris)
+
+let Balance=200
+let Withdraw= 10
+
+Balance=prompt("What is Your Balance?")
+Withdraw=prompt("How much will you be taking?")
+Balance=Balance-Withdraw
+console.log(Balance - Withdraw)
+alert ("Your balance is" + Balance - Withdraw)
+
+let grade= 80
+let extra= 5
+grade=prompt("What is Your Grade?")
+extra=prompt("How much Extra credit?")
+console.log(grade + extra)
+alert ("Your Grade is " + grade + extra)
+
+
+
+
+
+
+
+
